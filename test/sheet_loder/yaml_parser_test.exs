9fcf50f84a2bba7bdc2db78ad91462f222cf6f64
zigomir <zigomir@gmail.com>
2016-01-26 21:54:39 +0100

    Remove redundant test.

--- test/sheet_loder/yaml_parser_test.exs
+++ test/sheet_loder/yaml_parser_test.exs
@@ -1,26 +1,10 @@
 defmodule SheetLoader.YamlParserTest do
   use ExUnit.Case
 
-  test "Parse CSV to YAML key values" do
-    csv = """
-    title,SimpleSite v0.1
-    x,y
-    """
-
-    {:ok, %{en: yaml}} = SheetLoader.YamlParser.parse(0, [%GoogleSheets.WorkSheet{csv: csv, name: "en"}])
-
-    assert yaml == """
-    ---
-    en:
-      title: SimpleSite v0.1
-      x: y
-
-    """
-  end
-
-  test "Parse CSV to YAML lists" do
+  test "Parse CSV to YAML" do
     csv = """
     description,ready when you are
+    x,y
     people,[mirko, radirko, zobar]
     keys,\"[x, y, z]\"
     wadap,that's cool
@@ -32,6 +16,7 @@ defmodule SheetLoader.YamlParserTest do
     ---
     en:
       description: ready when you are
+      x: y
       people:
         - mirko
         - radirko
